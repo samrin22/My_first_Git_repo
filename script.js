8577eadbdcc07c9218480acba59773563592a092
samrin22 <samrinchoudhari@gmail.com>
2024-12-26 21:48:11 +0530

    new files added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listeners to buttons
+document.querySelector('.btn-join').addEventListener('click', () => {
+    alert('Join Now button clicked!');
+});
+
+document.querySelector('.btn-signup').addEventListener('click', () => {
+    alert('Sign Up button clicked!');
+});
